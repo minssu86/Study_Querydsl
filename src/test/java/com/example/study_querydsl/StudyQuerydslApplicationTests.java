99684f9a709d916bacd460ac5fa08ba98149f26a
minssu86 <manager.kim86@gmail.com>
2022-09-04 18:08:55 +0900

    Study QueryDSL : 생성

---
+++ src/test/java/com/example/study_querydsl/StudyQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
